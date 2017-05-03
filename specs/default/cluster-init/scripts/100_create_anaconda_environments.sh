f6509445f509616fb8ded320fa3f8e74204a8d9a
bwatrous <bwatrous@cyclecomputing.com>
2017-05-03 11:56:41 -0400

    automatically add a local conda envs dir for each user

--- specs/default/cluster-init/scripts/100_create_anaconda_environments.sh
+++ specs/default/cluster-init/scripts/100_create_anaconda_environments.sh
@@ -2,6 +2,22 @@
 
 source /etc/profile.d/anaconda-env.sh
 
+ANACONDA_ROOT=$( jetpack config anaconda.install_root )
+ANACONDA_VERSION=$( jetpack config anaconda.version )
+ANACONDA_HOME=${ANACONDA_ROOT}/${ANACONDA_VERSION}
+
+# Set up the condarc for all users:
+cat <<EOF > /etc/profile.d/anaconda_setup.sh
+source /etc/profile.d/anaconda-env.sh
+
+export ANACONDA_HOME=${ANACONDA_ROOT}/${ANACONDA_VERSION}
+
+if [ ! -f ~/.condarc ]; then
+  conda config --add envs_dirs \${HOME}/.conda/envs
+fi
+EOF
+chmod a+rx /etc/profile.d/anaconda_setup.sh
+
 YML_ENVS=$( find ${CYCLECLOUD_SPEC_PATH}/files/conda_envs -name "*yml" -type f )
 
 for YML in ${YML_ENVS}; do
